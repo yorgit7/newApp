4e2339824dc61800d2c73332f1c0c3a68797b1cd
yorgit7 <yorgit7@gmail.com>
2023-06-25 18:19:42 +0300

    Add js

---
+++ script.js
@@ -0,0 +1,10 @@
+var option = document.querySelector("#left");
+var option2 = document.querySelector("#right");
+
+
+option.addEventListener("click", function(){
+    console.log("left");
+})
+option2.addEventListener("click", function(){
+    console.log("right");
+})
